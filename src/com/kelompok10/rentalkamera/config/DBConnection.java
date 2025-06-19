d8bdc0ed981ab3c08bd2c280d4c703e7ac6338af
Haikal <haikalrafif354@gmail.com>
2025-06-19 21:27:16 +0700

    minor change DBConnection
    ganti rental_db ke rental_kamera

--- src/com/kelompok10/rentalkamera/config/DBConnection.java
+++ src/com/kelompok10/rentalkamera/config/DBConnection.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 
 public class DBConnection {
 
-    private static final String URL = "jdbc:mysql://localhost:3306/rental_db";
+    private static final String URL = "jdbc:mysql://localhost:3306/rental_kamera";
     private static final String USER = "root";
     private static final String PASSWORD = "";
     private static Connection connection;
